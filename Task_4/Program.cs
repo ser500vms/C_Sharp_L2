7fa1f3dd217dd4e2a3b58746d694f8569bc44605
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-05 20:08:20 +0300

    done task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Нахождение элемента массива равного заданному пользователем числу из одномерного массива.
+
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // это значит прирви если нашел ближайшее число
+    }
+    index++;
+}
